a23c65bc2c9b642f389b73cef808f1cd68676314
Trongnhat191 <nhat.nt215625@sis.hust.edu.vn>
2025-01-14 20:42:46 +0700

    increase resolution

--- car_check.py
+++ car_check.py
@@ -4,8 +4,19 @@ from ultralytics import YOLO
 import cv2 
 import numpy as np
 from paddleocr import PaddleOCR
+import json
 
-SOURCE_video2 = np.array([[123, 256],[321, 713],[917, 359],[501, 120]])
+def increase_resolution(plate_img, scale_factor=2):
+    # Tăng kích thước ảnh bằng super resolution
+    # Sử dụng INTER_CUBIC cho kết quả tốt với text
+    high_res = cv2.resize(plate_img, None, 
+                         fx=scale_factor, 
+                         fy=scale_factor, 
+                         interpolation=cv2.INTER_CUBIC)
+    
+    return high_res
+
+SOURCE_video = np.array([[123, 256],[321, 713],[917, 359],[501, 120]])
 ocr = PaddleOCR(lang='vi', show_log = False)
 plate_nums = []
 def parse_argument() -> argparse.Namespace:
@@ -41,10 +52,9 @@ if __name__ == "__main__":
     frame_generator = sv.get_video_frames_generator(args.source_video_path)
 
     polygon_zone = sv.PolygonZone(
-        SOURCE_video2,
+        SOURCE_video,
         # frame_resolution_wh=video_info.resolution_wh
     )
-
     for frame in frame_generator:
         result = car_model(frame)[0]
         detections = sv.Detections.from_ultralytics(result)
@@ -55,18 +65,28 @@ if __name__ == "__main__":
             # print(f"x_min: {int(detections.xyxy[0][0])}, y_min: {int(detections.xyxy[0][1])}, x_max: {int(detections.xyxy[0][2])}, y_max: {int(detections.xyxy[0][3])}")
             # print('-----')
             car_frame = frame[int(detections.xyxy[0][1]):int(detections.xyxy[0][3]), int(detections.xyxy[0][0]):int(detections.xyxy[0][2])]
-            plate_result = plate_model(car_frame)[0]
-            plate_detections = sv.Detections.from_ultralytics(plate_result)
-            plate_x_min = int(plate_detections.xyxy[0][0])-1
-            plate_y_min = int(plate_detections.xyxy[0][1])-1
-            plate_x_max = int(plate_detections.xyxy[0][2])+1
-            plate_y_max = int(plate_detections.xyxy[0][3])+1
-            cv2.imshow('plate', car_frame[plate_y_min:plate_y_max, plate_x_min:plate_x_max])
+            cv2.imshow('car', car_frame)
+            #save car image
+            # cv2.imwrite(f'cars/car{car_idx}.png', car_frame)
+            plate_result = plate_model(car_frame)
+            predictions_plate = json.loads(plate_result[0].to_json())
+            plate_x_min = int(predictions_plate[1]['box']['x1'])
+            plate_y_min = int(predictions_plate[1]['box']['y1'])
+            plate_x_max = int(predictions_plate[1]['box']['x2'])
+            plate_y_max = int(predictions_plate[1]['box']['y2'])
+            plate_img = car_frame[plate_y_min:plate_y_max, plate_x_min:plate_x_max]
+            #increase resolution
+            high_res_plate = increase_resolution(plate_img, scale_factor=2)
+            plate_img = high_res_plate
+
+            cv2.imshow('plate', plate_img)
+            # cv2.imshow('plate', car_frame[plate_y_min:plate_y_max, plate_x_min:plate_x_max])
             # print(f"x_min: {plate_x_min}, y_min: {plate_y_min}, x_max: {plate_x_max}, y_max: {plate_y_max}")
-            plate_crop_img = car_frame[plate_y_min:plate_y_max, plate_x_min:plate_x_max]
-            plate_num = ocr.ocr(plate_crop_img)
-            if plate_num != [None]:
-                plate_nums.append(plate_num[0][0][1][0])
+            # plate_crop_img = car_frame[plate_y_min:plate_y_max, plate_x_min:plate_x_max]
+            plate_num = ocr.ocr(plate_img)
+            print(plate_num)
+            # if plate_num != [None]:
+            plate_nums.append(plate_num[0][0][1][0])
 
             # print(plate_detections.data['class_name'][1])
         except:
@@ -79,7 +99,7 @@ if __name__ == "__main__":
             in detections.tracker_id
         ]
         annotated_frame = frame.copy()
-        annotated_frame = sv.draw_polygon(annotated_frame, polygon = SOURCE_video2, color = sv.Color(255, 0, 0), thickness = thickness)
+        annotated_frame = sv.draw_polygon(annotated_frame, polygon = SOURCE_video, color = sv.Color(255, 0, 0), thickness = thickness)
         annotated_frame = bounding_box_annotator.annotate(
             scene=annotated_frame, detections=detections
         )
--- test_easyocr.py
+++ test_easyocr.py
@@ -38,26 +38,68 @@ def visual_bbox(
     return img
 
 model_path = 'models/yolov8/detect/train/weights/best.pt'
-model = YOLO(model_path)
+model = YOLO('yolov8n.pt')#for car
+model1 = YOLO(model_path)#for plate
 
-img_path = 'images/car6.png'
+img_path = 'cars/car132.png'
 img = cv2.imread(img_path)
+cv2.imshow('img', img)
+
 results = model(img_path, verbose=False)
 # print(results)
-predictions = json.loads(results[0].to_json())
-# print(predictions[1])
-plate_x_min = int(predictions[1]['box']['x1'])-5
-plate_y_min = int(predictions[1]['box']['y1'])-5
-plate_x_max = int(predictions[1]['box']['x2'])+5
-plate_y_max = int(predictions[1]['box']['y2'])+5
-cv2.imshow('plate', img[plate_y_min:plate_y_max, plate_x_min:plate_x_max])
+# predictions = json.loads(results[0].to_json())
+# # print(predictions)
+# car_x_min = int(predictions[0]['box']['x1'])
+# car_y_min = int(predictions[0]['box']['y1'])
+# car_x_max = int(predictions[0]['box']['x2'])
+# car_y_max = int(predictions[0]['box']['y2'])
+# cv2.imshow('car', img[car_y_min:car_y_max, car_x_min:car_x_max])
 # print(plate_x_min, plate_y_min, plate_x_max, plate_y_max)
+# car_img = img[car_y_min:car_y_max, car_x_min:car_x_max]
+
+res_plate = model1(img)
+predictions_plate = json.loads(res_plate[0].to_json())
+print(predictions_plate)
+plate_x_min = int(predictions_plate[1]['box']['x1'])-1
+plate_y_min = int(predictions_plate[1]['box']['y1'])-1
+plate_x_max = int(predictions_plate[1]['box']['x2'])+1
+plate_y_max = int(predictions_plate[1]['box']['y2'])+1
+
 plate_img = img[plate_y_min:plate_y_max, plate_x_min:plate_x_max]
+cv2.imshow('plate', plate_img)
+
+
+
+import cv2
+import numpy as np
+
+def increase_resolution(plate_img, scale_factor=2):
+    # Tăng kích thước ảnh bằng super resolution
+    # Sử dụng INTER_CUBIC cho kết quả tốt với text
+    high_res = cv2.resize(plate_img, None, 
+                         fx=scale_factor, 
+                         fy=scale_factor, 
+                         interpolation=cv2.INTER_CUBIC)
+    
+    return high_res
+
+# Sử dụng hàm với scale_factor=2 để tăng gấp đôi độ phân giải
+high_res_plate = increase_resolution(plate_img, scale_factor=2)
+
+# Hiển thị kết quả
+cv2.imshow('Original Plate', plate_img)
+cv2.imshow('High Resolution Plate', high_res_plate)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Cập nhật biến plate_img với ảnh độ phân giải cao
+plate_img = high_res_plate
 res = ocr.ocr(plate_img)
-plate_num =  res[0][0][1][0]
-print(plate_num)
+print(res)
+# plate_num =  res[0][0][1][0]
+# print(plate_num)
 # cv2.imshow('plate_img', plate_img)
-img = visual_bbox(img_path, predictions, plate_num)
-cv2.imshow('img', img)
+# img = visual_bbox(img_path, predictions, plate_num)
+# cv2.imshow('img', img)
 key = cv2.waitKey(0)
 cv2.destroyAllWindows() 
